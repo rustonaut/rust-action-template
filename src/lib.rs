e89662d2db6faac8042a9451a49bc529e1f8be90
Philipp Korber <philipp@korber.dev>
2020-08-22 04:09:11 +0200

    Uses matrix for rust toolchaing and features.
    This also introduces a simple composite action
    called run-cargo which adapts (part of) cargos
    command line interface to github actions.

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,12 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[cfg(feature="yay")]
+    #[test]
+    fn yay_test() {}
+
+    #[cfg(feature="nay")]
+    #[test]
+    fn nay_test() {}
 }
